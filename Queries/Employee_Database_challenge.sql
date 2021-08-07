8caf682b442a4a7979b71c6fb800071d14a30aa0
LleeMcD <85351930+LleeMcD@users.noreply.github.com>
2021-08-07 18:16:03 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,5 +1,5 @@
 -- Deliverable 1 
--- 1. Create Retriement Titles table and export it to a csv file titled retirement_titles.
+-- 1. Create a Retirement Titles table and export it to a csv file titled retirement_titles.
 SELECT e.emp_no, 
 	e.first_name, 
 	e.last_name,
@@ -13,7 +13,7 @@ ON e.emp_no = t.emp_no
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
---2. -- Use Dictinct on with Orderby to remove duplicate rows. Export the table into a csv file labeled unique_titles
+--2. Use Dictinct on with Orderby to remove duplicate rows. Export the table into a csv file labeled unique_titles
 
 SELECT DISTINCT ON (emp_no) emp_no,
 	first_name,
