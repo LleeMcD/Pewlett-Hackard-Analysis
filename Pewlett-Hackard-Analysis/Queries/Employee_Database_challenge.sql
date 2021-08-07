e07e2d7344a67d1884ac57a313275ed5d09c74ef
LleeMcD <85351930+LleeMcD@users.noreply.github.com>
2021-08-07 10:07:12 -0500

    Challenge files updated

--- Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -49,5 +49,5 @@ ON e.emp_no = de.emp_no
 JOIN titles AS t
 ON e.emp_no = t.emp_no
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-     AND (de.to_date = '9999-01-01')
+     AND (de.to_date = '9999-01-01');
 ORDER BY emp_no;
