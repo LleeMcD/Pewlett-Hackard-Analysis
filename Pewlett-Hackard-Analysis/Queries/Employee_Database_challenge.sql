74b552ed084780996a0033302e405d16f8b7c12f
LleeMcD <85351930+LleeMcD@users.noreply.github.com>
2021-08-07 12:51:31 -0500

    Add files via upload

--- Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -49,5 +49,26 @@ ON e.emp_no = de.emp_no
 JOIN titles AS t
 ON e.emp_no = t.emp_no
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-     AND (de.to_date = '9999-01-01');
+     AND (de.to_date = '9999-01-01')
 ORDER BY emp_no;
+
+--Additional Queries
+
+--Create a query to find the number of additional employees eligible for the mentorship program based on birth years between 1965 and 1969.
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name, 	
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+t.title
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1969-12-31')
+     AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+
